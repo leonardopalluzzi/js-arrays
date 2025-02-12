4c07a5ac7fa9de2371fd2bd54b48f2f2a0d677ab
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-12 16:25:49 +0100

    fourth bonus task complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -37,7 +37,30 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+
+//counters 
+let j; // positive counter
+let k; // negative counter
+
+// chek fabio's presence 
+for (let i = 0; i < teachers.length; i++){
+  if (teachers[i] == 'Fabio'){
+    j++;
+  } else {
+    k++;
+  }
+}
+
+// output message
+if (j != 0){
+  flag = "Fabio e presente nell'elenco";
+} else {
+  flag = "Fabio non e presente nell'elenco";
+}
+
+const isFabioPresent = flag;
+console.log(isFabioPresent);
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
