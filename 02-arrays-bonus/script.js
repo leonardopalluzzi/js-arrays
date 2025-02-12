5dbb808135c4252a026490f7ea8c498b9d8f6aac
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-12 16:09:36 +0100

    third bonus task complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,6 +31,9 @@ console.log(longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf('Ed'), 1);
+console.log(teachers);
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
